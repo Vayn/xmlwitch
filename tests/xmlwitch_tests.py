a2591e281e9faecfef606d72a925c58d56b61ac5
Vayn a.k.a. VT <vayn@vayn.de>
2011-09-27 19:41:46 +0800

    2to3

--- tests/xmlwitch_tests.py
+++ tests/xmlwitch_tests.py
@@ -1,4 +1,5 @@
-from __future__ import with_statement
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8:
 
 import sys
 import os
@@ -13,38 +14,39 @@ sys.path.append(ROOT)
 import unittest
 import xmlwitch
 
+
 class XMLWitchTestCase(unittest.TestCase):
-    
+
     def expected_document(self, filename):
-        expected = os.path.join(ROOT, 'tests',  'expected',  filename)
+        expected = os.path.join(ROOT, 'tests', 'expected', filename)
         with open(expected) as document:
             return document.read()
-            
+
     def test_simple_document(self):
             xml = xmlwitch.Builder(version='1.0', encoding='utf-8')
             with xml.person:
                 xml.name("Bob")
                 xml.city("Qusqu")
-            self.assertEquals(
-                str(xml), 
+            self.assertEqual(
+                str(xml),
                 self.expected_document('simple_document.xml')
             )
-    
+
     def test_utf8_document(self):
-        string = u"""An animated fantasy film from 1978 based on the first """ \
-                 u"""half of J.R.R Tolkien\u2019s Lord of the Rings novel. The """ \
-                 u"""film was mainly filmed using rotoscoping, meaning it was """ \
-                 u"""filmed in live action sequences with real actors and then """ \
-                 u"""each frame was individually animated."""
+        string = "An animated fantasy film from 1978 based on the first " \
+                 "half of J.R.R Tolkien\u2019s Lord of the Rings novel. The " \
+                 "film was mainly filmed using rotoscoping, meaning it was " \
+                 "filmed in live action sequences with real actors and then " \
+                 "each frame was individually animated."
         xml = xmlwitch.Builder(version='1.0', encoding='utf-8')
         with xml.test:
              xml.description(string)
-        
-        self.assertEquals(
+
+        self.assertEqual(
             str(xml),
             self.expected_document('utf8_document.xml')
         )
-    
+
     def test_nested_elements(self):
         xml = xmlwitch.Builder()
         with xml.feed(xmlns='http://www.w3.org/2005/Atom'):
@@ -58,37 +60,37 @@ class XMLWitchTestCase(unittest.TestCase):
                 xml.id('urn:uuid:1225c695-cfb8-4ebb-aaaa-80da344efa6a')
                 xml.updated('2003-12-13T18:30:02Z')
                 xml.summary('Some text.')
-        self.assertEquals(
-            str(xml), 
+        self.assertEqual(
+            str(xml),
             self.expected_document('nested_elements.xml')
         )
-    
+
     def test_rootless_fragment(self):
         xml = xmlwitch.Builder()
         xml.data(None, value='Just some data')
-        self.assertEquals(
-            str(xml), 
+        self.assertEqual(
+            str(xml),
             self.expected_document('rootless_fragment.xml')
         )
-    
+
     def test_content_escaping(self):
         xml = xmlwitch.Builder()
         with xml.doc:
             xml.item('Text&to<escape', some_attr='attribute&value>to<escape')
-        self.assertEquals(
-            str(xml), 
+        self.assertEqual(
+            str(xml),
             self.expected_document('content_escaping.xml')
         )
-    
+
     def test_namespaces(self):
         xml = xmlwitch.Builder()
-        with xml.parent(**{'xmlns:my':'http://example.org/ns/'}):
+        with xml.parent(**{'xmlns:my': 'http://example.org/ns/'}):
             xml.my__child(None, my__attr='foo')
-        self.assertEquals(
-            str(xml), 
+        self.assertEqual(
+            str(xml),
             self.expected_document('namespaces.xml')
-        )        
-    
+        )
+
     def test_atom_feed(self):
         xml = xmlwitch.Builder(version="1.0", encoding="utf-8")
         with xml.feed(xmlns='http://www.w3.org/2005/Atom'):
@@ -107,8 +109,8 @@ class XMLWitchTestCase(unittest.TestCase):
                     with xml.div(xmlns='http://www.w3.org/1999/xhtml'):
                         xml.label('Some label', for_='some_field')
                         xml.input(None, type='text', value='')
-        self.assertEquals(
-            str(xml), 
+        self.assertEqual(
+            str(xml),
             self.expected_document('atom_feed.xml')
         )
 
